87ae85590f4e8f553a962aec86f5183f12d9abde
GavrilS <gavr1lll@abv.bg>
2025-04-06 23:56:31 +0300

    Meats list

---
+++ categories/meats.py
@@ -0,0 +1,30 @@
+meat_list = {
+    'chicken breast': {
+        'measurment': 100,
+        'calories': 165
+    },
+    'pork': {
+        'measurment': 100,
+        'calories': 242
+    },
+    'beef': {
+        'measurment': 100,
+        'calories': 267
+    },
+    'bison': {
+        'measurment': 100,
+        'calories': 138
+    },
+    'deer': {
+        'measurment': 100,
+        'calories': 149
+    },
+    'wild boar': {
+        'measurment': 100,
+        'calories': 160
+    },
+    'turkey': {
+        'measurment': 100,
+        'calories': 163
+    }
+}
