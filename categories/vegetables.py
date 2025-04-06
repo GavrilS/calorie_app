d9e858759d9df0c17990a78897e82dc209dad111
GavrilS <gavr1lll@abv.bg>
2025-04-06 23:45:23 +0300

    Added vegetable list with calories per cup

---
+++ categories/vegetables.py
@@ -0,0 +1,102 @@
+vegetable_list = {
+    'lettuce': {
+        'measurment': 36,
+        'calories': 5
+    },
+    'spinach': {
+        'measurment': 30,
+        'calories': 7
+    },
+    'celery': {
+        'measurment': 100,
+        'calories': 14
+    },
+    'cucumber': {
+        'measurment': 100,
+        'calories': 16
+    },
+    'zucchini': {
+        'measurment': 113,
+        'calories': 19
+    },
+    'radish': {
+        'measurment': 116,
+        'calories': 18
+    },
+    'mushroom': {
+        'measurment': 96,
+        'calories': 21
+    },
+    'cabbage': {
+        'measurment': 90,
+        'calories': 22
+    },
+    'cauliflower': {
+        'measurment': 107,
+        'calories': 27
+    },
+    'asparagus': {
+        'measurment': 134,
+        'calories': 27
+    },
+    'pumpkin': {
+        'measurment': 116,
+        'calories': 30
+    },
+    'green beans': {
+        'measurment': 100,
+        'calories': 30
+    },
+    'broccoli': {
+        'measurment': 91,
+        'calories': 31
+    },
+    'tomatoes': {
+        'measurment': 180,
+        'calories': 36
+    },
+    'turnips': {
+        'measurment': 130,
+        'calories': 36
+    },
+    'bell peppers': {
+        'measurment': 135,
+        'calories': 38
+    },
+    'brussels prouts': {
+        'measurment': 88,
+        'calories': 38
+    },
+    'winter squash': {
+        'measurment': 116,
+        'calories': 40
+    },
+    'onion': {
+        'measurment': 115,
+        'calories': 46
+    },
+    'carrots': {
+        'measurment': 128,
+        'calories': 53
+    },
+    'artichoke': {
+        'measurment': 128,
+        'calories': 60
+    },
+    'potato': {
+        'measurment': 150,
+        'calories': 116
+    },
+    'peas': {
+        'measurment': 145,
+        'calories': 118
+    },
+    'sweet potato': {
+        'measurment': 150,
+        'calories': 129
+    },
+    'corn': {
+        'measurment': 164,
+        'calories': 177
+    }
+}
