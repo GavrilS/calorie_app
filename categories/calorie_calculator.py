34a3eff2646bef7234f369f7b94d50987dc30fb3
GavrilS <gavr1lll@abv.bg>
2025-04-07 20:15:59 +0300

    Added calculation logic per ingredient

--- categories/calorie_calculator.py
+++ categories/calorie_calculator.py
@@ -5,8 +5,21 @@ from meats import meat_list
 from vegetables import vegetable_list
 
 def calculate_calories(meal):
+    total_calories = 0
     for item in meal.items():
         print(item)
+
+
+def _calories_per_portion(ingredient, portion_grams, category):
+    if 'meat' in category:
+        source = meat_list
+    else:
+        source = vegetable_list
+    
+    grams = source.get(ingredient).get('measurement')
+    calories = source.get(ingredient).get('calories')
+    total_calories = calories * (portion_grams / grams)
+    return total_calories
 
 
 if __name__=='__main__':
