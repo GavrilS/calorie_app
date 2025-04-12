2dff7d74ffbcabce8da533271a24c559516b4008
GavrilS <gavr1lll@abv.bg>
2025-04-12 15:19:22 +0300

    Added home route and fixed wrong reference

--- categories/calorie_calculator.py
+++ categories/calorie_calculator.py
@@ -2,8 +2,8 @@
 A helper to calculate calories from lists of user supplied ingredients/meals.
 """
 import math
-from meats import meat_list
-from vegetables import vegetable_list
+from categories.meats import meat_list
+from categories.vegetables import vegetable_list
 
 def calculate_calories(meal):
     total_calories = 0
@@ -29,13 +29,3 @@ def _calories_per_portion(ingredient, portion_grams, category):
 
     total_calories = calories * (portion_grams / grams)
     return total_calories
-
-
-if __name__=='__main__':
-    meal = {
-        'steak': {'grams': 250, 'category': 'meat'},
-        'chicken breast': {'grams': 100, 'category': 'meat'},
-        'cauliflower': {'grams': 200, 'category': 'vegetables'}
-    }
-
-    print(calculate_calories(meal))
