5c6d60788332bed60182e34086153dc78a217252
GavrilS <gavr1lll@abv.bg>
2025-04-13 12:06:40 +0300

    Added secret key

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import uuid
 from calorie_app import create_app, calculator
 
 
 def main():
     app = create_app()
     app.register_blueprint(calculator.bp)
+
+    secret_uuid = uuid.uuid4().hex
+    app.secret_key = secret_uuid
 
     app.run(debug=True)
 
