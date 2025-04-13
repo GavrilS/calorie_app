2bde11fb2f3beebaa2add03d11b8034072a81267
GavrilS <gavr1lll@abv.bg>
2025-04-13 12:53:01 +0300

    Added apm config/dependecies to the project to test ingesting apm data in Elastic

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import uuid
 from calorie_app import create_app, calculator
 
+from elasticapm.contrib.flask import ElasticAPM
+
 
 def main():
     app = create_app()
@@ -8,6 +10,8 @@ def main():
 
     secret_uuid = uuid.uuid4().hex
     app.secret_key = secret_uuid
+
+    apm = ElasticAPM(app, service_name='calorie_app', debug=True)
 
     app.run(debug=True)
 
